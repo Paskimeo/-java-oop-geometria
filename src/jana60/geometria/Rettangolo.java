947ced4023b12213fe4a03f1876516ac68afc8e1
paskimeo <paskimeo@gmail.com>
2022-06-14 15:11:13 +0200

    aggiunto metodi

--- src/jana60/geometria/Rettangolo.java
+++ src/jana60/geometria/Rettangolo.java
@@ -2,4 +2,24 @@ package jana60.geometria;
 
 public class Rettangolo {
 
+	//attributi
+	int altezza;
+	int larghezza;
+	
+	//metodi
+	
+	int area () {
+		
+		return larghezza * altezza;
+	}
+	
+	int perimetro () {
+		
+		return (larghezza + altezza) * 2;
+	}
+	
+	String risulato() {
+		
+		return "l'area del rettangolo è: " + area() + " il perimetro del rettangolo è:" + perimetro();
+	}
 }
