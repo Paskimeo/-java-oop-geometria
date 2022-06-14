c6e8fbd7a77f4f9384129c58ba769e5ebccb6290
paskimeo <paskimeo@gmail.com>
2022-06-14 15:26:55 +0200

    inserto insert dati e calcolo soluzione

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,10 +1,25 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// aggiunto scanner e rettangolo
+		Scanner scan = new Scanner(System.in);
+		Rettangolo ret = new Rettangolo();
+	
+		
+		//richiesta altezza e larghezza
+		System.out.println("inserisci l'altezza del rettangolo:");
+		ret.altezza= Integer.parseInt(scan.nextLine());
 
+		System.out.println("inserisci la larghezza del rettangolo:");
+		ret.larghezza= Integer.parseInt(scan.nextLine());
+		
+		System.out.println(ret.risulato());
+		scan.close();
 	}
 
 }
